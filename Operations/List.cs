eb005ca3e12af9faefb2a7f8a27381d1ab74c7b7
Emirhan Kaaner <kaaner4mir@gmail.com>
2025-12-20 20:17:40 +0300

    docs: redesign text

--- Operations/List.cs
+++ Operations/List.cs
@@ -7,7 +7,7 @@
         foreach (var item in Data._accounts)
         {
             Utils.WriteColored($"{lm.T("Id").PadRight(25)}: {item.AccountId}", ConsoleColor.DarkYellow);
-            Utils.WriteColored($"{lm.T("Name").PadRight(25)}: {item.AccountName}", ConsoleColor.DarkYellow);
+            Utils.WriteColored($"{lm.T("Name").PadRight(26)}: {item.AccountName}", ConsoleColor.DarkYellow);
             Utils.WriteColored($"{lm.T("Branch").PadRight(25)}: {item.Branch}", ConsoleColor.DarkGray);
             Utils.WriteColored($"{lm.T("OpeningDate").PadRight(25)}: {item.OpeningDate}", ConsoleColor.DarkCyan);
             Utils.WriteColored($"{lm.T("Currency").PadRight(25)}: {item.Currency}", ConsoleColor.White);
@@ -21,7 +21,7 @@
         foreach (var item in Data._foreignaccounts)
         {
             Utils.WriteColored($"{lm.T("Id").PadRight(25)}: {item.AccountId}", ConsoleColor.DarkYellow);
-            Utils.WriteColored($"{lm.T("Name").PadRight(25)}: {item.Branch}", ConsoleColor.DarkYellow);
+            Utils.WriteColored($"{lm.T("Name").PadRight(26)}: {item.Branch}", ConsoleColor.DarkYellow);
             Utils.WriteColored($"{lm.T("Currency").PadRight(25)}: {item.Currency}", ConsoleColor.White);
             Utils.WriteColored($"{new string('-', 50)}", ConsoleColor.White);
         }
